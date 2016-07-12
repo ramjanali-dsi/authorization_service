faaf416ccfc5c84e3a2a5ca4e8d1cd416cb4909e
sabbir <sabbirahmed.dsi@gmail.com>
2016-07-12 14:29:46 +0600

    add user session crud api.

--- src/main/java/com/dsi/authorization/resource/UserSessionResource.java
+++ src/main/java/com/dsi/authorization/resource/UserSessionResource.java
@@ -3,7 +3,6 @@ package com.dsi.authorization.resource;
 import com.dsi.authorization.exception.CustomException;
 import com.dsi.authorization.exception.ErrorContext;
 import com.dsi.authorization.exception.ErrorMessage;
-import com.dsi.authorization.model.User;
 import com.dsi.authorization.model.UserSession;
 import com.dsi.authorization.service.UserSessionService;
 import com.dsi.authorization.service.impl.UserSessionServiceImpl;
@@ -17,10 +16,7 @@ import org.apache.log4j.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -55,9 +51,111 @@ public class UserSessionResource {
             userSession.setVersion(1);
 
             userSessionService.saveUserSession(userSession);
-            logger.info("User session save success.");
+            logger.info("User session save successfully.");
 
             responseObj.put(Constants.MESSAGE, "Create user session success.");
+            return Response.ok().entity(responseObj.toString()).build();
+
+        } catch (JSONException je){
+            ErrorContext errorContext = new ErrorContext(null, null, je.getMessage());
+            ErrorMessage errorMessage = new ErrorMessage(Constants.AUTHORIZATION_SERVICE_0009,
+                    Constants.AUTHORIZATION_SERVICE_0009_DESCRIPTION, errorContext);
+            throw new CustomException(errorMessage);
+        }
+    }
+
+    @PUT
+    @ApiOperation(value = "User Session Update", notes = "User Session Update", position = 2)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "User session update success"),
+            @ApiResponse(code = 500, message = "User session update failed, unauthorized.")
+    })
+    public Response updateUserSession(String requestBody) throws CustomException {
+        JSONObject responseObj = new JSONObject();
+        JSONObject requestObj;
+        try{
+            logger.info("Request Body:: " + requestBody);
+            requestObj = new JSONObject(requestBody);
+            String userID = Utility.validation(requestObj, "userId");
+            String accessToken = Utility.validation(requestObj, "accessToken");
+            String newAccessToken = Utility.validation(requestObj, "newAccessToken");
+
+            UserSession userSession = userSessionService.
+                    getUserSessionByUserIdAndAccessToken(userID, accessToken);
+
+            logger.info("New accessToken: " + newAccessToken);
+
+            userSession.setAccessToken(newAccessToken);
+            userSession.setModifiedBy(userID);
+            userSession.setModifiedDate(Utility.today());
+            userSession.setVersion(1);
+
+            userSessionService.updateUserSession(userSession);
+            logger.info("User session update successfully.");
+
+            responseObj.put(Constants.MESSAGE, "Update user session success.");
+            return Response.ok().entity(responseObj.toString()).build();
+
+        } catch (JSONException je){
+            ErrorContext errorContext = new ErrorContext(null, null, je.getMessage());
+            ErrorMessage errorMessage = new ErrorMessage(Constants.AUTHORIZATION_SERVICE_0009,
+                    Constants.AUTHORIZATION_SERVICE_0009_DESCRIPTION, errorContext);
+            throw new CustomException(errorMessage);
+        }
+    }
+
+    @DELETE
+    @ApiOperation(value = "Delete User Session", notes = "Delete User Session", position = 3)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Delete user session success"),
+            @ApiResponse(code = 500, message = "Delete user session failed, unauthorized.")
+    })
+    public Response deleteUserSession(String requestBody) throws CustomException {
+        JSONObject responseObj = new JSONObject();
+        JSONObject requestObj;
+        try {
+            logger.info("Request Body:: " + requestBody);
+            requestObj = new JSONObject(requestBody);
+            String userID = Utility.validation(requestObj, "userId");
+            String accessToken = Utility.validation(requestObj, "accessToken");
+
+            UserSession userSession = userSessionService.
+                    getUserSessionByUserIdAndAccessToken(userID, accessToken);
+
+            userSessionService.deleteUserSession(userSession);
+            logger.info("Delete user session successfully.");
+
+            responseObj.put(Constants.MESSAGE, "Delete user session success");
+            return Response.ok().entity(responseObj.toString()).build();
+
+        } catch (JSONException je){
+            ErrorContext errorContext = new ErrorContext(null, null, je.getMessage());
+            ErrorMessage errorMessage = new ErrorMessage(Constants.AUTHORIZATION_SERVICE_0009,
+                    Constants.AUTHORIZATION_SERVICE_0009_DESCRIPTION, errorContext);
+            throw new CustomException(errorMessage);
+        }
+    }
+
+    @POST
+    @Path("/is_valid")
+    @ApiOperation(value = "Get Valid User Session", notes = "Get Valid User Session", position = 4)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Get valid user session success"),
+            @ApiResponse(code = 500, message = "Get valid user session failed, unauthorized.")
+    })
+    public Response getLoginSession(String requestBody) throws CustomException {
+        JSONObject responseObj = new JSONObject();
+        JSONObject requestObj;
+        try {
+            logger.info("Request Body:: " + requestBody);
+            requestObj = new JSONObject(requestBody);
+            String userID = Utility.validation(requestObj, "userId");
+            String accessToken = Utility.validation(requestObj, "accessToken");
+
+            userSessionService.getUserSessionByUserIdAndAccessToken(userID, accessToken);
+            logger.info("Valid user session found.");
+
+            responseObj.put(Constants.MESSAGE, "Success");
             return Response.ok().entity(responseObj.toString()).build();
 
         } catch (JSONException je){
